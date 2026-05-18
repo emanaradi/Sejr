1285e8585cec0ba0e5f0f87f06f0a1fccc99f2bc
emanaradi <aradieman@gmail.com>
2026-05-18 16:27:50 +0300

    added a theme file

---
+++ constants/theme.ts
@@ -0,0 +1,12 @@
+export const Colors = {
+  light: {
+    text: "#000",
+    background: "#fff",
+    tint: "#0f766e",
+  },
+  dark: {
+    text: "#fff",
+    background: "#000",
+    tint: "#0f766e",
+  },
+};
