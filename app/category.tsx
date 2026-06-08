5873abc22dc3eeea295c152e4f0ebfca61c9a119
emanaradi <aradieman@gmail.com>
2026-06-08 14:22:34 +0300

    added the offers list in the category page

--- app/category.tsx
+++ app/category.tsx
@@ -1,17 +1,18 @@
+import { router, useLocalSearchParams } from "expo-router";
+import React from "react";
 import {
-  ScrollView,
+  FlatList,
+  Image,
   StyleSheet,
   Text,
-  View,
-  Image,
   TouchableOpacity,
+  View,
 } from "react-native";
-import React from "react";
-import { useLocalSearchParams, router } from "expo-router";
-import { Offer } from "../lib/data";
+
+import { ImageBackground } from "expo-image";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { screenHeight, screenWidth } from "../lib/data";
 import { icons } from "../constants";
+import { Offer, screenHeight } from "../lib/data";
 
 const category = () => {
   const { id, category, offers, image } = useLocalSearchParams();
@@ -19,35 +20,58 @@ const category = () => {
   let parsedOffers: Offer[] = [];
 
   try {
-    const offersString = Array.isArray(offers) ? offers[0] : offers;
-    parsedOffers = offersString ? JSON.parse(offersString) : [];
-  } catch {
+    if (typeof offers === "string") {
+      parsedOffers = JSON.parse(offers);
+    } else if (Array.isArray(offers)) {
+      parsedOffers = JSON.parse(offers[0]);
+    } else {
+      parsedOffers = [];
+    }
+  } catch (error) {
+    console.log("JSON Parse Error:", error);
     parsedOffers = [];
   }
 
   return (
-    <ScrollView>
-      <SafeAreaView style={{ display: "flex", gap: 20 }}>
-        <View style={styles.header}>
-          <TouchableOpacity
-            onPress={() => {
-              router.push("/home");
-            }}
-          >
-            <Image style={styles.backicon} source={icons.backicon} />
-          </TouchableOpacity>
-          <Text style={[styles.font, { fontSize: 18, fontWeight: "bold" }]}>
-            {category}
-          </Text>
-        </View>
-      </SafeAreaView>
-    </ScrollView>
+    <SafeAreaView style={styles.safeArea}>
+      <View style={styles.header}>
+        <TouchableOpacity
+          onPress={() => {
+            router.push("/home");
+          }}
+        >
+          <Image style={styles.backicon} source={icons.backicon} />
+        </TouchableOpacity>
+        <Text style={[styles.font, { fontSize: 18, fontWeight: "bold" }]}>
+          {category}
+        </Text>
+      </View>
+      <View style={styles.offersList}>
+        <FlatList
+          horizontal={false}
+          keyExtractor={(item) => item.id.toString()}
+          data={parsedOffers}
+          renderItem={({ item }) => (
+            <TouchableOpacity>
+              <ImageBackground source={item.image} style={styles.offer}>
+                <Text style={styles.offerText}>{item.store}</Text>
+                <Text style={styles.offerText}>{item.offer}</Text>
+              </ImageBackground>
+            </TouchableOpacity>
+          )}
+        />
+      </View>
+    </SafeAreaView>
   );
 };
 
 export default category;
 
 const styles = StyleSheet.create({
+  safeArea: {
+    display: "flex",
+    gap: 10,
+  },
   header: {
     display: "flex",
     flexDirection: "row",
@@ -67,5 +91,28 @@ const styles = StyleSheet.create({
     display: "flex",
     flexDirection: "column",
     gap: 15,
+  },
+  offersList: {
+    padding: 25,
+    display: "flex",
+    gap: 10,
+  },
+  offer: {
+    display: "flex",
+    flexDirection: "row",
+    gap: 15,
+    marginBottom: 20,
+    alignItems: "flex-end",
+    height: screenHeight * 0.14,
+    boxShadow: "0 0 5px grey",
+    borderRadius: 20,
+    overflow: "hidden",
+    justifyContent: "space-around",
+  },
+  offerText: {
+    fontFamily: "Tommy-Bold",
+    color: "white",
+    fontWeight: "700",
+    padding: 15,
   },
 });
